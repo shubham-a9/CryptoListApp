b1426f5b095265756e4ac4fddd0018c0e210bcb9
Anurag Shubham <shubham.a@northeastern.edu>
2023-02-10 13:59:19 -0500

    Initial commit

---
+++ CryptoListViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoListViewApp
+//
+//  Created by Anurag Shubham on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoListViewApp/CryptoListViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListViewAppApp.swift
+//  CryptoListViewApp
+//
+//  Created by Anurag Shubham on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
